21f5cef5cee9390d13b195c2cb41bd5a801a9237
chenyu-max <2497191408@qq.com>
2021-07-09 16:22:53 +0800

    常用中间件

--- routes/init.js
+++ routes/init.js
@@ -1,19 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express(); //创建一个express应用
-
-app.use(require("./staticMiddleware"));
-
-app.get("/news/abc", (req, res, next) => {
-    console.log("handler1");
-    // throw new Error("abc")
-    // 相当于 next(new Error("abc"))
-    next(new Error("abc"));
-    //   next();
-});
-
-//能匹配  /news  /news/abc   /news/123   /news/ab/adfs
-//不能匹配  /n   /a   /   /newsabc
-app.use("/news", require("./errorMiddleware"));
+const staticRoot = path.resolve(__dirname, '../public');
+/**
+ * 下面这段代码的作用：
+ * 当请求时，会根据请求路径(req.path)，从指定的目录中寻找是否存在该文件，如果存在，直接响应文件内容，而不再移交给后续的中间件
+ * 如果不存在文件，则直接移交给后续的中间件处理
+ * 默认情况下，如果映射的结果是一个目录，则会自动使用index.html文件
+ */
+app.use(express.static(staticRoot));
+app.use(require("./errorMiddleware"));
 
 const port = 5008;
 app.listen(port, () => {
